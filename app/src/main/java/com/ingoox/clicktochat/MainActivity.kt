e580b05bdf6742459ddef4f4e3e51d87523f2b6d
pablocarracci <pablocarracci@gmail.com>
2021-05-20 22:24:53 -0300

    add files of version 0.3.2

--- app/src/main/java/com/ingoox/clicktochat/MainActivity.kt
+++ app/src/main/java/com/ingoox/clicktochat/MainActivity.kt
@@ -1,26 +1,36 @@
 package com.ingoox.clicktochat
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import com.ingoox.clicktochat.ui.theme.OpenChatTheme
+import java.net.URLEncoder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,53 +48,124 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
+    val context = LocalContext.current
     var phone by rememberSaveable { mutableStateOf("") }
     var message by rememberSaveable { mutableStateOf("") }
+    var link by rememberSaveable { mutableStateOf("") }
 
-    // Event handlers
+    fun onPhoneChange(value: String) {
+        phone = value
+        link = createLink(phone, message)
+    }
+
+    fun onMessageChange(value: String) {
+        message = value
+        link = createLink(phone, message)
+    }
+
+    fun clearPhoneHandler() {
+        phone = ""
+        link = createLink(phone, message)
+    }
+
+    fun clearMessageHandler() {
+        message = ""
+        link = createLink(phone, message)
+    }
 
     Content(
         phone = phone,
         message = message,
-        onPhoneChange = { phone = it },
-        onMessageChange = { message = it })
+        link = link,
+        onPhoneChange = { onPhoneChange(it) },
+        onMessageChange = { onMessageChange(it) },
+        openChatHandler = { openLink(link, context) },
+        shareLinkHandler = { shareLink(link, context) },
+        clearMessageHandler = { clearMessageHandler() },
+        clearPhoneHandler = { clearPhoneHandler() }
+    )
 }
 
 @Composable
 fun Content(
     phone: String,
     message: String,
+    link: String,
     onPhoneChange: (String) -> Unit,
-    onMessageChange: (String) -> Unit
+    onMessageChange: (String) -> Unit,
+    openChatHandler: () -> Unit,
+    shareLinkHandler: () -> Unit,
+    clearMessageHandler: () -> Unit,
+    clearPhoneHandler: () -> Unit
 ) {
     Column(
         modifier = Modifier
             .fillMaxHeight()
-            .padding(32.dp),
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center
     ) {
+        val focusRequester = FocusRequester()
+
         Card(
             modifier = Modifier.fillMaxWidth(),
-            elevation = 2.dp
+            elevation = 1.dp
         ) {
-            Text(
-                "wa.me/$phone",
-                Modifier.padding(16.dp)
-            )
+            Column {
+                Text(
+                    text = stringResource(R.string.card_title),
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 0.dp)
+                )
+                Text(
+                    text = link,
+                    modifier = Modifier
+                        .padding(16.dp, 8.dp, 16.dp, 8.dp)
+                        .animateContentSize(),
+                    maxLines = 5,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
         }
-        Spacer(modifier = Modifier.size(16.dp))
+        Spacer(Modifier.size(16.dp))
 
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = phone,
             onValueChange = { onPhoneChange(it) },
-            label = { Text("Phone") }
+            label = { Text(stringResource(R.string.phone_label)) },
+            trailingIcon = {
+                if (phone.isNotEmpty()) {
+                    ClearTextIcon(
+                        onClick = { clearPhoneHandler() },
+                        contentDescription = stringResource(R.string.clear_phone_description)
+                    )
+                }
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Phone,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { focusRequester.requestFocus() }
+            )
         )
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester),
             value = message,
             onValueChange = { onMessageChange(it) },
-            label = { Text("Message") }
+            label = { Text(stringResource(R.string.message_label)) },
+            trailingIcon = {
+                if (message.isNotEmpty()) {
+                    ClearTextIcon(
+                        onClick = { clearMessageHandler() },
+                        contentDescription = stringResource(R.string.clear_message_description)
+                    )
+                }
+            },
+            maxLines = 6
         )
         Spacer(Modifier.size(16.dp))
 
@@ -92,25 +173,64 @@ fun Content(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.End
         ) {
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text("GENERATE LINK")
+            OutlinedButton(onClick = { shareLinkHandler() }) {
+                Text(stringResource(R.string.share_link_button))
             }
             Spacer(Modifier.size(8.dp))
-            Button(onClick = { /*onLinkHandle?*/ }) {
-                Text("OPEN CHAT")
+
+            Button(onClick = { openChatHandler() }) {
+                Text(stringResource(R.string.open_chat_button))
             }
         }
     }
 }
 
-fun createLink(phoneNumber: String, message: String): String {
-    return ""
+@Composable
+fun ClearTextIcon(
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    contentDescription: String = "Clear text"
+) {
+    IconButton(
+        onClick = { onClick() },
+        enabled = enabled
+    ) {
+        Icon(
+            imageVector = Icons.Rounded.Clear,
+            contentDescription = contentDescription
+        )
+    }
 }
 
-fun openLink(link: String) {
-
+fun createLink(phone: String, message: String): String {
+    val phoneDigits = phone.filter { it.isDigit() }
+    if (phoneDigits.isBlank() and message.isBlank()) return ""
+    var link = "https://wa.me/"
+    if (phoneDigits.isNotBlank()) {
+        link += phoneDigits
+    }
+    if (message.isNotBlank()) {
+        val encodedMessage = URLEncoder.encode(message, "UTF-8")
+        link += "?text=$encodedMessage"
+    }
+    return link
 }
 
-fun shareLink(link: String) {
+fun openLink(link: String, context: Context) {
+    if (link.isNotBlank()) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        startActivity(context, intent, null)
+    }
+}
 
+fun shareLink(link: String, context: Context) {
+    if (link.isNotBlank()) {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, link)
+            type = "text/plain"
+        }
+        val chooserIntent = Intent.createChooser(intent, "")
+        startActivity(context, chooserIntent, null)
+    }
 }
--- app/src/main/java/com/ingoox/clicktochat/ui/theme/Theme.kt
+++ app/src/main/java/com/ingoox/clicktochat/ui/theme/Theme.kt
@@ -7,24 +7,24 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    primary = Teal200,
+    primaryVariant = Teal700,
+    secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
+    primary = Teal500,
+    primaryVariant = Teal700,
+    secondary = Teal200
 
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    /* Other default colors to override
+background = Color.White,
+surface = Color.White,
+onPrimary = Color.White,
+onSecondary = Color.Black,
+onBackground = Color.Black,
+onSurface = Color.Black,
+*/
 )
 
 @Composable
@@ -36,9 +36,9 @@ fun OpenChatTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
     }
 
     MaterialTheme(
-            colors = colors,
-            typography = Typography,
-            shapes = Shapes,
-            content = content
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
     )
 }
